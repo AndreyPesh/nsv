3ab0daf189999d9da0036a426e68a7ae029fe166
Andrey Peshko <coursestud@mail.ru>
2023-04-28 18:38:48 +0300

    feat: simple routes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-const user: Array<number> = [1];
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
 
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.get('/', (req, res) => res.send({ user: 'User' }));
+
+app.get('/user/:id', (req, res) => {
+  console.log(req.params);
+  res.send({ user: 'User' });
+});
+
+app.all('*', (req, res) => {
+  res.sendStatus(404);
+});
+
+app.listen(PORT, () => console.log(`Server start on ${PORT} port`));
