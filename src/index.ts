26c5e29a598f50048d53a00afde6ec87c23f84ab
Andrei <coursestud@mail.ru>
2023-06-08 16:46:58 +0300

    init: init app

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-import app from './app/app';
-
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => console.log(`Server start on ${PORT} port`));
--- src/routes/auth.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { handleLogin } from '../controller/authController';
-
-const router = Router();
-
-router.post('/', handleLogin);
-
-export default router;
