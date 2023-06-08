1ae3259b2ecb63c499517c6ffa86432714be241e
Andrei <coursestud@mail.ru>
2023-06-08 18:55:56 +0300

    feat: add register method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @UsePipes(new ValidationPipe())
   @HttpCode(HttpStatus.OK)
   @Post('register')
-  async register() {
-    return this.authService.register();
+  async register(@Body() dto: AuthDto) {
+    return this.authService.register(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { faker } from '@faker-js/faker';
+import { hash } from 'argon2';
+import { PrismaService } from 'src/prisma.service';
+import { AuthDto } from './auth.dto';
 
 @Injectable()
 export class AuthService {
-  register() {
-    return 'Register user';
+  constructor(private prisma: PrismaService) {}
+  async register(dto: AuthDto) {
+    const isUserExist = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    console.log(dto);
+
+    if (isUserExist) throw new BadRequestException('User already exist');
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        name: faker.person.firstName(),
+        avatarPath: faker.image.avatar(),
+        phone: faker.phone.number('+ 375 (##) ### ## ##'),
+        password: await hash(dto.password),
+      },
+    });
+    return user;
   }
 }
