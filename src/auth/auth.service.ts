526adf3ac55d9f997efe965771e4e4dbf6732efd
Andrei <coursestud@mail.ru>
2023-06-09 22:05:09 +0300

    feat: add refresh token method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,42 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { faker } from '@faker-js/faker';
 import { hash } from 'argon2';
 import { PrismaService } from 'src/prisma.service';
-import { AuthDto } from './auth.dto';
+import { AuthDto } from './dto/auth.dto';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService, private jwt: JwtService) {}
+
+  async getNewToken(refreshToken: string) {
+    const result = await this.jwt.verifyAsync(refreshToken);
+    if (result) throw new UnauthorizedException('Invalid refresh token');
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: result.id,
+      },
+    });
+
+    const tokens = await this.issueToken(user.id);
+
+    return {
+      user: this.returnUserFields(user),
+      ...tokens,
+    };
+  }
+
+  async login(dto: AuthDto) {
+    const user = await this.validateUser(dto);
+  }
+
   async register(dto: AuthDto) {
     const isUserExist = await this.prisma.user.findUnique({
       where: {
@@ -27,7 +55,13 @@ export class AuthService {
         password: await hash(dto.password),
       },
     });
-    return user;
+
+    const tokens = await this.issueToken(user.id);
+
+    return {
+      user: this.returnUserFields(user),
+      ...tokens,
+    };
   }
 
   private async issueToken(userId: number) {
@@ -47,7 +81,17 @@ export class AuthService {
   private returnUserFields(user: User) {
     return {
       id: user.id,
-      email: user.email
-    }
+      email: user.email,
+    };
+  }
+
+  private async validateUser(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) throw new NotFoundException('User not found');
   }
 }
