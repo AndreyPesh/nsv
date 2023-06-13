bcf125bbe5c7015b343484d066a433fd96683ff8
Andrei <coursestud@mail.ru>
2023-06-13 21:51:07 +0300

    feat: add review service

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { returnReviewObject } from './return-review.object';
+import { ReviewDto } from './review.dto';
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+  constructor(private prisma: PrismaService) {}
+
+  async getAll() {
+    return await this.prisma.review.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+      select: returnReviewObject,
+    });
+  }
+
+  async create(userId: number, dto: ReviewDto, productId: number) {
+    return await this.prisma.review.create({
+      data: {
+        ...dto,
+        product: {
+          connect: {
+            id: productId,
+          },
+        },
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+  }
+
+  async getAverageValueByProductId(productId: number) {
+    return this.prisma.review
+      .aggregate({
+        where: {
+          id: productId,
+        },
+        _avg: { rating: true },
+      })
+      .then((data) => data._avg);
+  }
+
+  async delete(id: number) {
+    return await this.prisma.review.delete({
+      where: { id },
+    });
+  }
+}
