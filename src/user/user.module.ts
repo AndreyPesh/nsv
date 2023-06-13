658ef0913d6d5153ca2d61ea5ea2f8f4ca11d894
Andrey Peshko <coursestud@mail.ru>
2023-06-13 15:32:33 +0300

    docs: add seeder

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,7 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaService } from 'src/prisma.service';
-import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from 'src/auth/jwt.strategy';
-import { AuthModule } from 'src/auth/auth.module';
 import { ConfigService } from '@nestjs/config';
 
 @Module({
