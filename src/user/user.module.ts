de9f7b3527a4054ec20d16ed5b494e7fa12070a4
Andrei <coursestud@mail.ru>
2023-06-13 22:43:09 +0300

    feat: implement order module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -8,5 +8,6 @@ import { ConfigService } from '@nestjs/config';
 @Module({
   controllers: [UserController],
   providers: [UserService, ConfigService, PrismaService, JwtStrategy],
+  exports: [UserService],
 })
 export class UserModule {}
