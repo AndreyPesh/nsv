14080d7390d61c00a93fd4541f8581fbc71479a1
Andrey Peshko <coursestud@mail.ru>
2023-06-14 17:07:31 +0300

    feat: implement product service

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,7 +1,46 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ProductService } from './product.service';
 
-@Controller('product')
+@Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Get()
+  async getAll() {
+    return this.productService.getAll();
+  }
+
+  @Get('by-slug/:slug')
+  async getBySlug(@Param('slug') slug: string) {
+    return this.productService.bySlug(slug);
+  }
+
+  @Get(':id')
+  @Auth()
+  async getById(@Param('id') id: string) {
+    return this.productService.byId(+id);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(HttpStatus.OK)
+  @Auth()
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() dto: ProductDto) {
+    return this.productService.update(+id, dto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Auth()
+  @Post()
+  async create() {
+    return this.productService.create();
+  }
+
+  // @UsePipes(new ValidationPipe())S
+  @HttpCode(HttpStatus.OK)
+  @Auth()
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.productService.delete(+id);
+  }
 }
